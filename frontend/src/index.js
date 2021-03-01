c9cf26309162d0bcd67facbb03cc7f8d79479e9a
RolandWarburton <warburtonroland@gmail.com>
2021-03-01 14:07:39 +1100

    :bug: Fix warning caused by React strict mode and MUI

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,10 +4,12 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
+// When https://github.com/mui-org/material-ui/issues/13394 gets fixed, replace the <> tags with <React.strict>
+// Until then this fixes the findDOMNode is deprecated in StrictMode warning https://github.com/Yuvaleros/material-ui-dropzone/issues/250 in MUI
 ReactDOM.render(
-	<React.StrictMode>
+	<>
 		<App />
-	</React.StrictMode>,
+	</>,
 	document.getElementById("root")
 );
 
